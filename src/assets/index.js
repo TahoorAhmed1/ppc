4604d190fbba1ab61a9ce721e4ca73ffe7ffda85
shahmeer319 <shahmeergithub@gmail.com>
2025-04-20 00:37:10 +0500

    done

--- src/assets/index.js
+++ src/assets/index.js
@@ -53,6 +53,22 @@ import serviceIcon4 from "@/assets/images/servies-icon (4).svg";
 import serviceIcon5 from "@/assets/images/servies-icon (5).svg";
 import serviceIcon6 from "@/assets/images/servies-icon (6).svg";
 import fap from "@/assets/images/faq-background.png";
+import project1 from "@/assets/images/project/suburban-website.jpg";
+import project2 from "@/assets/images/project/kidz-website.jpg";
+import project3 from "@/assets/images/project/alan.jpg";
+import project4 from "@/assets/images/project/blb media.jpg";
+import project5 from "@/assets/images/project/capshok.jpg";
+import project6 from "@/assets/images/project/clarion.jpg";
+import project7 from "@/assets/images/project/event.jpg";
+import project8 from "@/assets/images/project/futrad.jpg";
+import project9 from "@/assets/images/project/gonaine chatbox.jpg";
+import project10 from "@/assets/images/project/jason.jpg";
+import project11 from "@/assets/images/project/margrita.png";
+import project12 from "@/assets/images/project/monique.jpg";
+import project13 from "@/assets/images/project/piza.jpg";
+import project14 from "@/assets/images/project/skin.jpg";
+import project15 from "@/assets/images/project/timothy.jpg";
+import project16 from "@/assets/images/project/yohan it.jpg";
 
 export {
   logo,
@@ -110,4 +126,20 @@ export {
   wixIcon,
   webflowIcon,
   wordpressIcon,
+  project1,
+  project2,
+  project3,
+  project4,
+  project5,
+  project6,
+  project7,
+  project8,
+  project9,
+  project10,
+  project11,
+  project12,
+  project13,
+  project14,
+  project15,
+  project16,
 };
---
+++ src/components/project.tsx
@@ -0,0 +1,55 @@
+
+import { project1,
+    project2,
+    project3,
+    project4,
+    project5,
+    project6,
+    project7,
+    project8,
+    project9,
+    project10,
+    project11,
+    project12,
+    project13,
+    project14,
+    project15,
+    project16, } from "@/assets";
+import ProjectCard from "./project-card";
+const ProjectCardData: { img: string, title: string, link: string }[] = []
+
+const data = [
+    project1,
+      project2,
+      project3,
+      project4,
+      project5,
+      project6,
+      project7,
+      project8,
+      project9,
+      project10,
+      project11,
+      project12,
+      project13,
+      project14,
+      project15,
+      project16,
+]
+
+
+const Projects = () => {
+    return (
+        <div className="wrapper px-4">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 place-items-center">
+                {data?.map((project, index) => {
+                    return (
+                        <ProjectCard img={project} key={index} />
+                    )
+                })}
+            </div>
+        </div>
+    );
+};
+
+export default Projects;
