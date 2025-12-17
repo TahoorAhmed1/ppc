ef67e82e6ab3b04c81ac342f6561544c16b35d43
Taha Zaheer Khan <31067691+musaafir101@users.noreply.github.com>
2025-12-17 15:15:19 +0500

    fixed

--- src/components/stats-card.tsx
+++ src/components/stats-card.tsx
@@ -1,10 +1,10 @@
-"use client"
+"use client";
 
-import { motion } from "framer-motion"
+import { motion } from "framer-motion";
 
 export default function StatsBar() {
   return (
-    <div className="flex   max-w-3xl border border-black mx-auto items-center justify-between rounded-[1.6vw] bg-gradient-to-r from-green-200 via-teal-200 to-blue-200 p-[1.6vw]">
+    <div className="flex   max-w-3xl border border-black mx-auto items-center justify-between rounded-[1.6vw] bg-gradient-to-r from-black via-slate-200 to-slate-100 p-[1.6vw]">
       {[
         { value: "300M+", label: "Global Impressions\nAcross Platforms" },
         { value: "1,250+", label: "Fresh Content\nCreated Daily" },
