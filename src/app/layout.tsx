b05a7ab5bfbc6de18e252ef26491ea9d8bd43636
Taha Zaheer Khan <31067691+musaafir101@users.noreply.github.com>
2025-12-18 18:42:04 +0500

    fixed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -54,15 +54,15 @@ export default function RootLayout({
 
         <Script id="tawkto-widget" strategy="afterInteractive">
           {`
-            var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
-            (function(){
-              var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
-              s1.async = true;
-              s1.src = 'https://embed.tawk.to/681e77a508bed819150db3fb/1iqrg6nra';
-              s1.charset = 'UTF-8';
-              s1.setAttribute('crossorigin', '*');
-              s0.parentNode.insertBefore(s1, s0);
-            })();
+          var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/69440416b0232f1983a9727d/1jcoqvum2';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
           `}
         </Script>
       </head>
